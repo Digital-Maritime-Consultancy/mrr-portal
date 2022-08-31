a78ef3064b53104f0924844e03e8160c23c9d486
Jinki Jung <your.jinki.jung@gmail.com>
2022-08-31 13:51:06 +0200

    introduction of env variables

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div style={{ padding: "2rem"}}>
-        <h1>Maritime Resource Registry Portal</h1>
+        <h1>Maritime Resource Registry Management Portal</h1>
       </div>
       <nav
         style={{
@@ -13,8 +13,8 @@ function App() {
           paddingBottom: "1rem",
         }}
       >
-        <Link to="searchView">Search</Link> |{" "}
-        <Link to="treeView">Graph</Link>
+        <Link to="searchView">Search by MRN</Link> |{" "}
+        <Link to="treeView">See MRN graph</Link>
       </nav>
       <Outlet />
     </div>
