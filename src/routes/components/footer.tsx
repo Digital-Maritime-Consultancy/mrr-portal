411545a058493a7ac95891791993b029c9e90e2d
Jinki Jung <your.jinki.jung@gmail.com>
2022-09-28 12:44:15 +0200

    query when user press enter or lookup button

--- src/routes/components/footer.tsx
+++ src/routes/components/footer.tsx
@@ -1,7 +1,7 @@
 import { Row } from "react-bootstrap";
 
 export const Footer = () =>
-    <Row>
+    <Row className="pt-5">
         <div style={{ fontSize: "80%", paddingTop: "1rem"}}>
             The development of MRR is a part of the project titled “Development of Open Platform Technologies for Smart Maritime Safety and Industries”<br />funded by the Korea Research Institute of Ships and Ocean Engineering (PES4070).
         </div>
