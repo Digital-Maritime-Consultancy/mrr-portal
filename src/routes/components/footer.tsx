5f01d95a7b0e1e95ff561aec3b74ba1d4da4268e
Jinki <your.jinki.jung@gmail.com>
2023-11-23 14:45:53 +0100

    operational context included

--- src/routes/components/footer.tsx
+++ src/routes/components/footer.tsx
@@ -3,6 +3,6 @@ import { Row } from "react-bootstrap";
 export const Footer = () =>
     <Row className="pt-5">
         <div style={{ fontSize: "80%", paddingTop: "1rem"}}>
-            The development of MRR is a part of the project titled “Development of Open Platform Technologies for Smart Maritime Safety and Industries”<br />funded by the Korea Research Institute of Ships and Ocean Engineering (PES4070).
+            The development of MRR is a part of the project titled “Development of Open Platform Technologies for Smart Maritime Safety and Industries” (PES4070)<br />funded by the Korea Research Institute of Ships and Ocean Engineering (KRISO). This environment is currently operated by <a href="https://www.kriso.re.kr/eng/" target="_blank">KRISO</a> on behalf of <a href="https://www.iala-aism.org/" target="_blank">IALA</a>.
         </div>
     </Row>
